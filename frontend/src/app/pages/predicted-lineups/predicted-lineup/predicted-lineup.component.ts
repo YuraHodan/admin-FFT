5c3f4d31f9a5ce107b14825533e9eba7948ffd64
YuraHodan <yura.hodan@gmail.com>
2025-03-25 13:30:42 +0200

    feat: add tour data loading to predicted lineup page
    - Add tour data fetching using getTourById
    - Display basic tour information (number, dates, status)
    - Handle tour loading errors
    - Implement identical approach as tour.component.ts

--- frontend/src/app/pages/predicted-lineups/predicted-lineup/predicted-lineup.component.ts
+++ frontend/src/app/pages/predicted-lineups/predicted-lineup/predicted-lineup.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Tour } from '../../../models/tour.interface';
+import { ToursService } from '../../../services/tours.service';
 
 @Component({
   selector: 'app-predicted-lineup',
@@ -8,6 +11,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './predicted-lineup.component.html',
   styleUrls: ['./predicted-lineup.component.scss']
 })
-export class PredictedLineupComponent {
-  // Component logic will go here
+export class PredictedLineupComponent implements OnInit {
+  tourId?: string;
+  tour?: Tour;
+
+  constructor(
+    private route: ActivatedRoute,
+    private toursService: ToursService
+  ) {
+    this.tourId = this.route.snapshot.paramMap.get('id') || undefined;
+  }
+
+  ngOnInit() {
+    if (this.tourId) {
+      this.toursService.getTourById(this.tourId).subscribe({
+        next: (tour: Tour) => {
+          this.tour = tour;
+        },
+        error: (error) => {
+          console.error('Error loading tour:', error);
+          // TODO: Add error handling
+        }
+      });
+    }
+  }
 } 
