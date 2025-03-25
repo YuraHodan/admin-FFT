0eea165d52720928ec7c405b2a308fd1eecfdaa3
YuraHodan <yura.hodan@gmail.com>
2025-03-25 13:14:36 +0200

    feat: add tours list to predicted lineups page
    - Add tours list display to predicted lineups component
    - Use same implementation approach as schedule component
    - Show tour number, dates and status for each tour

--- frontend/src/app/pages/predicted-lineups/predicted-lineups.component.ts
+++ frontend/src/app/pages/predicted-lineups/predicted-lineups.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Tour } from '../../models/tour.interface';
+import { ToursService } from '../../services/tours.service';
 
 @Component({
   selector: 'app-predicted-lineups',
@@ -8,6 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './predicted-lineups.component.html',
   styleUrls: ['./predicted-lineups.component.scss']
 })
-export class PredictedLineupsComponent {
-  // Component logic will go here
+export class PredictedLineupsComponent implements OnInit {
+  tours: Tour[] = [];
+
+  constructor(private toursService: ToursService) {}
+
+  ngOnInit(): void {
+    this.loadTours();
+  }
+
+  private loadTours(): void {
+    this.toursService.getTours().subscribe(tours => {
+      this.tours = tours;
+    });
+  }
 } 
